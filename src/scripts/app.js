77e1dddf49edc344c4da474e86ba8e9bddf2192c
gilletth-dev <gilletthibault23@gmail.com>
2026-05-24 12:46:56 +0200

    header

--- src/scripts/app.js
+++ src/scripts/app.js
@@ -1,1 +1,33 @@
-console.info('Hello world');
+var menuBtn = document.querySelector('.header__btn');
+var menu = document.querySelector('.header__menu');
+var menuLinks = document.querySelectorAll('.header__el a');
+menuBtn.addEventListener('click', menuOpen);
+
+function menuOpen() {
+  menu.classList.toggle('header__menu--open');
+  menuBtn.classList.toggle('header__btn--open');
+
+  if (menu.classList.contains('header__menu--open')) {
+    document.body.style.overflow = 'hidden';
+  } else {
+    document.body.style.overflow = 'auto';
+  }
+}
+
+function closeMenu() {
+  menu.classList.remove('header__menu--open');
+  menuBtn.classList.remove('header__btn--open');
+  document.body.style.overflow = 'auto';
+}
+
+menuLinks.forEach(function (link) {
+  link.addEventListener('click', closeMenu);
+});
+
+document.addEventListener('click', function(e){
+  if (menu.classList.contains('header__menu--open')) {
+    if (!menu.contains(e.target) && !menuBtn.contains(e.target)) {
+      closeMenu();
+    }
+  }
+});
