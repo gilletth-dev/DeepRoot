43535e42ca780350431de8927c424a915a1970dd
gilletth-dev <gilletthibault23@gmail.com>
2026-05-30 16:03:05 +0200

    Phase final

--- src/scripts/app.js
+++ src/scripts/app.js
@@ -1,5 +1,6 @@
-"use strict"
+"use strict";
 
+// MENU
 var menuBtn = document.querySelector('.header__btn');
 var menu = document.querySelector('.header__menu');
 var menuLinks = document.querySelectorAll('.header__el a');
@@ -33,3 +34,24 @@ document.addEventListener('click', function(e){
     }
   }
 });
+
+var header = document.querySelector('.header');
+let oldScrollY = 0;
+
+window.addEventListener("scroll", scrollListener);
+function scrollListener() {
+  const maxScroll = document.body.scrollHeight - window.innerHeight;
+  const currentScroll = window.scrollY;
+
+  const isScrollingUp = oldScrollY > currentScroll;
+  const isAtTop = currentScroll === 0;
+  const isAtBottom = currentScroll === maxScroll;
+
+  if(isScrollingUp || isAtTop || isAtBottom){
+    header.classList.remove("header--up");
+  } else {
+    header.classList.add("header--up");
+  }
+
+  oldScrollY = currentScroll
+}
